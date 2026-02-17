7ea5d271c3958e97b0c4f6b64ab79612a3627a7f
Elena D Nikolaeva <lene.nikolaeva@gmail.com>
2026-02-17 12:19:53 +0300

    Update inventory_balance.sql

--- inventory_balance.sql
+++ inventory_balance.sql
@@ -1,3 +1,8 @@
+-- Business task: Inventory Balance
+-- Metric: Stock Quantity
+-- Description: Lists products with low stock for replenishment
+-- Grain: product level
+
 SELECT 
     product_name,
     availability
